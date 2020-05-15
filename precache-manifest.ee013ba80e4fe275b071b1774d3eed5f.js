c832027de03aeccca36062fc9c178cfc759ad7d6
jack <jackbisceglia2000@gmail.com>
2020-05-15 18:23:04 -0400

    Updates

---
+++ precache-manifest.ee013ba80e4fe275b071b1774d3eed5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3f7055294548bd1c16bb3351675091",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "8b633ae5bb461b03010a",
+    "url": "/trello-clone/static/css/main.fd195ff8.chunk.css"
+  },
+  {
+    "revision": "206111d0d499fa679fcb",
+    "url": "/trello-clone/static/js/2.84957a39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trello-clone/static/js/2.84957a39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b633ae5bb461b03010a",
+    "url": "/trello-clone/static/js/main.bab83716.chunk.js"
+  },
+  {
+    "revision": "c3f8567e36ec02ede2a4",
+    "url": "/trello-clone/static/js/runtime-main.5a52d3f5.js"
+  },
+  {
+    "revision": "0f55b63e085b48cfbcfca61d84555758",
+    "url": "/trello-clone/static/media/icons8-edit.0f55b63e.svg"
+  }
+]);
