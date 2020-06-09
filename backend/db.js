d0617117a4d0771ce0ff34d771ea85db4914244a
jack <jackbisceglia2000@gmail.com>
2020-06-09 17:00:51 -0400

    SQLite queries/functions created

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,63 @@
-const express = require('express');
-const sqlite3 = require('sqlite3');
-const Sequelize = require('sequelize')
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./main.db3');  
 
-const db = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'C:/Programming/trello-clone/backend/main.sqlite'
-});
 
+// ---- Make Tables based on models ----
+const createCardTable = () => {
+    db.serialize(() =>  {  
+        db.run("CREATE TABLE IF NOT EXISTS Card (cardTitle TEXT, cardId INTEGER)");  
+    });
+}
+const createTaskTable = () => {
+    db.serialize(() =>  {  
+        db.run("CREATE TABLE IF NOT EXISTS Task (taskTitle TEXT, taskId INTEGER)");  
+    });
+}
+
+// ---- Insert to tables ----
+const addCard = (title, id) => {
+    db.serialize(() => {  
+        db.run(`INSERT into Card(cardTitle,cardId) VALUES ("${title}",${id})`);
+    })
+}
+const addTask = (title, id) => {
+    db.serialize(() => {  
+        db.run(`INSERT into Task(taskTitle,taskId) VALUES ("${title}",${id})`);
+    })
+}
+
+// ---- Updated tables ----
+// - Cards -
+const updateCardTitle = (oldTitle, newTitle) => {
+    db.run(`UPDATE Card SET cardTitle = "${newTitle}" WHERE cardTitle = "${oldTitle}"`); 
+}
+
+const updateCardId = (oldId, newId) => {
+    db.run(`UPDATE Card SET cardId = ${newId} WHERE cardId = ${oldId}`); 
+}
+
+// - Tasks -
+const updateTaskTitle = (oldTitle, newTitle) => {
+    db.run(`UPDATE Task SET taskTitle = "${newTitle}" WHERE taskTitle ="${oldTitle}"`); 
+}
+
+const updateTaskId = (oldId, newId) => {
+    db.run(`UPDATE Task SET taskId = ${newId} WHERE taskId = ${oldId}`); 
+}
+
+// ---- Updated tables ----
+// - Cards -
+const deleteCard = (titleToDel) => {
+    db.run(`DELETE FROM Card WHERE cardTitle = "${titleToDel}"`); 
+}
+
+// - Tasks -
+const deleteTask = (titleToDel) => {
+    db.run(`DELETE FROM Task WHERE taskTitle = "${titleToDel}"`); 
+}
+
+addTask("Task 2", 145);
+addTask("Task 3", 171);
+addTask("Task 4", 778);
 
 module.exports = db;
