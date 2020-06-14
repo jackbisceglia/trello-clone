ecddab8fcacb2f4bc1aa9f4801760e42d442a334
jack <jackbisceglia2000@gmail.com>
2020-06-13 21:48:52 -0400

    React Router Added, Landing Page Added, Sign Up Modal added

---
+++ react-frontend/src/Landing.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
+
+import './App.css';
+import './landing.css';
+
+const modalStyles = {
+    content : {
+        width                 : '30%',
+        height                 : '50%',
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)',
+        display : "flex",
+        flexDirection : "column",
+        alignItems : "center"
+      }
+}
+
+Modal.setAppElement('#root')
+
+export default function Landing({history}) {
+
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const closeModal = () => {
+        setModalOpen(false)
+    }
+
+    return (
+        <div className="main-container">
+            <Modal
+                isOpen={modalOpen}
+                onRequestClose={closeModal}
+                style={modalStyles}
+                contentLabel="Example Modal"
+                >
+                <h1 style={{textAlign: 'center', fontSize: '1.5rem'}}>Sign Up for Trello Clone!</h1>
+                <form action="" className="sign-up">
+                    <input type="text" placeholder="Email Address"/>
+                    <input type="text" placeholder="Password"/>
+                    <input type="text" placeholder="Confirm Password"/>
+                    <button className="button sign-up-btn">Sign Up!</button>
+                </form>
+            </Modal>
+
+
+            <div className="intro">
+                <h1 style={{textAlign: 'center', color: 'white', margin: '1rem 0', marginTop: '4rem', fontSize: '2.5rem'}}>Trello Clone</h1>
+
+                <h2 style={{textAlign: 'center', margin: '1rem 0', lineHeight: '1.7rem'}}>Simple.<br/>Organized.<br/>Accessible.</h2>
+
+                <h3 style={{textAlign: 'center', width: '40%', fontWeight: '200', color: 'white', fontSize: '1.25rem', margin: '1rem 0'}}>Task tracking for your everyday needs</h3>
+            </div>
+            <div className="login">
+                <div className="login-box">
+                    <h1 className="title">Login</h1>
+                    <form className="login-form" action="" onSubmit={() => {
+                        history.push("/home")
+                    }}>
+                        <input type="text" placeholder="Email address" className="inputBox"/>
+                        <input type="text" placeholder="Password" className="inputBox"/>
+                        <button className="button">Login</button>
+                    </form>
+                    <p className="else" onClick={() => {
+                        setModalOpen(true)
+                    }}>Sign Up Here</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
---
+++ react-frontend/src/Trello.js
@@ -0,0 +1,210 @@
+import React, {useState, useEffect} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import './App.css';
+import './Components/card.css';
+import Navbar from './Components/Navbar';
+import Card from './Components/Card';
+
+function Trello() {
+  const [cards, setCards] = useState([]);
+
+  const [tasks, setTasks] = useState([]);
+
+
+  // Cards Call
+  useEffect(() => {
+    fetch('/cards')
+    .then(res => res.json())
+    .then(data => {
+      setCards([...cards, ...data]);
+    })
+    }, 
+  []);
+
+  // Tasks Call
+  useEffect(() => {
+    fetch('/tasks')
+    .then(res => res.json())
+    .then(data => {
+      setTasks([...tasks, ...data]);
+      })
+    }, 
+  []);
+
+
+  // ------ CARD'S STATE ------
+  // Update Card Title && Update State
+  const updateCardTitle = (cardId, newName) => {
+    let edit = cards.slice();
+    console.log(edit);
+    edit.forEach(currCard => {
+      if (currCard.cardid === cardId){
+        console.log(currCard.cardTitle)
+        currCard.cardtitle = newName;
+      }
+    })
+    setCards(edit);
+
+    fetch(`/cards/${cardId}`, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        cardTitle: newName
+      })
+    });
+  }
+
+  const addCard = () => {
+    let nextCard = {
+      cardtitle : `New Card (Update)`,
+      cardid : uuidv4(),
+    };
+
+    setCards([...cards, nextCard]);
+
+    fetch(`/cards`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        cardId: nextCard.cardid,
+        cardTitle: nextCard.cardtitle
+      })
+    });
+  }
+
+  const deleteCard = (cardId) => {
+    setTasks(tasks.filter(currTask => currTask.parentid !== cardId));
+    setCards(cards.filter(currCard => currCard.cardid !== cardId));
+
+    fetch(`/cards/${cardId}`, {
+      method: 'DELETE'
+      });
+  }
+  
+
+  // ------ TASK'S STATE ------
+  const updateTaskTitle = (taskId, newName) => {
+    let edit = tasks.slice();
+    edit.forEach(currTask => {
+      if (currTask.taskid === taskId){
+        currTask.tasktitle = newName
+      }
+    })
+    setTasks(edit);
+
+    fetch(`/tasks/${taskId}`, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        tasktitle: newName
+      })
+    });
+  }
+
+  // Add Task to Card & Update State
+  const addTask = (parentCardId, addedTitle) => {
+    const newTask = {
+        taskid : uuidv4(),
+        tasktitle : addedTitle,
+        completed: false,
+        parentid : parentCardId,
+      };
+
+      setTasks([...tasks, newTask]);
+
+      fetch(`/tasks`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newTask)
+      });
+  }
+
+  const deleteTask = (taskId) => {
+    setTasks(tasks.filter(currTask => currTask.taskid !== taskId));
+
+    fetch(`/tasks/${taskId}`, {
+      method: 'DELETE'
+      });
+  }
+
+  const strikeTask = (taskId) => {
+    let edit = tasks.slice();
+    edit.forEach(currTask => {
+      if (currTask.taskid === taskId){
+        let isStruck = currTask.completed;
+        currTask.completed = !isStruck;
+
+        fetch(`/tasks/completed/${taskId}`, {
+          method: 'PUT',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          }
+        });
+      }
+    })
+    setTasks(edit);
+
+  }
+
+
+
+
+  // **** UI ****
+  return (
+    <>
+    <Navbar /> 
+    <div className="contain">
+      
+      {/* CARDS */}
+      {cards.map(currCard => (
+        <Card
+          key={currCard.cardid}
+
+          cardTitle={currCard.cardtitle}
+          taskList={tasks.filter(curr => curr.parentid === currCard.cardid)}
+          cardId={currCard.cardid}
+
+          // Card Functions
+          updateCardTitle={updateCardTitle}
+          deleteCard={deleteCard}
+
+          // Task Functions
+          updateTaskTitle={updateTaskTitle}
+          addTask={addTask}
+          deleteTask={deleteTask}
+          strikeTask={strikeTask}
+          
+
+        />
+      ))}
+
+      {/* NEW CARD */}
+      <button className="new-list" onClick={() => addCard()}>+</button>
+      <div className="padding-div"></div>
+      {/* TEMP - DELETE */}
+      {/* <button onClick={() => {
+        console.table(cards)
+        }}>See Cards</button>
+      <button onClick={() => {
+        console.table(tasks)
+        }}>See Tasks</button> */}
+    </div>
+    </>
+  );
+}
+
+export default Trello;
